1af34bfb7953ad9a4ec42508dd8620e50f17cb5c
sher304 <esherovshermat@gmail.com>
2021-07-29 20:49:14 +0600

    CBV search, paginations

--- main/urls.py
+++ main/urls.py
@@ -3,13 +3,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('category/<str:slug>', category_detail, name='category'),
-    path('recipe-detail/<int:pk>/', recipe_detail, name='detail'),
+    path('', MainPageView.as_view(), name='home'),
+    path('category/<str:slug>', CategoryDetailView.as_view(), name='category'),
+    path('recipe-detail/<int:pk>/', RecipeDetailView.as_view(), name='detail'),
     path('add-recipe/', add_recipe, name='add-recipe'),
     path("update-recipe/<int:pk>", update_recipe, name='update-recipe'),
-    path('delete-recipe/<int:pk>', delete_recipe, name='delete-recipe'),
+    path('delete-recipe/<int:pk>', DeleteRecipeView.as_view(), name='delete-recipe'),
 ]
+
 
 
 
--- main/views.py
+++ main/views.py
@@ -1,19 +1,72 @@
+from datetime import timedelta
+
+from django.db.models import Q
 from django.forms import modelformset_factory
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.views.generic import ListView, DetailView, DeleteView
 
 from .forms import RecipeForm, ImageForm
 from .models import *
 
+#
+# def index(request):
+#     recipes = Recipe.objects.all()
+#     return render(request, 'index.html', {'recipes': recipes})
 
-def index(request):
-    return render(request, 'index.html')
+
+class MainPageView(ListView):
+    model = Recipe
+    template_name = 'index.html'
+    context_object_name = 'recipes'
+    paginate_by = 2
 
 
-def category_detail(request, slug):
-    category = Category.objects.get(slug=slug)
-    recipes = Recipe.objects.filter(category_id=slug)
-    return render(request, 'category-detail.html', locals())
+    def get_template_names(self):
+        template_name = super(MainPageView, self).get_template_names()
+        search = self.request.GET.get('q')
+        if search:
+            template_name = 'search.html'
+        return template_name
+
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        search = self.request.GET.get('q')
+        if search:
+            context['recipes'] = Recipe.objects.filter(Q(title__icontains=search) |
+                                                       Q(description__icontains=search))
+        elif filter:
+            start_date = timezone.now() - timedelta(days=1)
+            context['recipes'] = Recipe.objects.filter(created__gte=start_date)
+        else:
+            context['recipes'] = Recipe.objects.all()
+        return context
+
+
+# def category_detail(request, slug):
+#     category = Category.objects.get(slug=slug)
+#     recipes = Recipe.objects.filter(category_id=slug)
+#     return render(request, 'category-detail.html', locals())
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'category-detail.html'
+    context_object_name = 'category'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.slug = kwargs.get('slug', None)
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recipes'] = Recipe.objects.filter(category_id=self.slug)
+        return context
 
 
 def recipe_detail(request, pk):
@@ -21,6 +74,18 @@ def recipe_detail(request, pk):
     image = recipe.get_image
     images = recipe.images.exclude(id=image.id)
     return render(request, 'recipe_detail.html', locals())
+
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe_detail.html'
+    context_object_name = 'recipe'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        image = self.get_object().get_image
+        context['images'] = self.get_object().images.exclude(id=image.id)
+        return context
 
 
 def add_recipe(request):
@@ -57,12 +122,25 @@ def update_recipe(request, pk):
     return render(request, 'update-recipe.html', locals() )
 
 
-def delete_recipe(request, pk):
-    recipe = get_object_or_404(Recipe, pk=pk)
-    if request.method == 'POST':
-        recipe.delete()
+# def delete_recipe(request, pk):
+#     recipe = get_object_or_404(Recipe, pk=pk)
+#     if request.method == 'POST':
+#         recipe.delete()
+#         messages.add_message(request, messages.SUCCESS, 'DELETED!')
+#         return redirect('home')
+#     return render(request, 'delete-recipe.html')
+#
+#
+
+class DeleteRecipeView(DeleteView):
+    model = Recipe
+    template_name = 'delete-recipe.html'
+    success_url = reverse_lazy('home')
+
+    def delete(self, request, *args, **kwargs):
+
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        self.object.delete()
         messages.add_message(request, messages.SUCCESS, 'DELETED!')
-        return redirect('home')
-    return render(request, 'delete-recipe.html')
-
-
+        return HttpResponseRedirect(success_url)
